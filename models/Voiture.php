399373cfac32e77999f4471f118d25cf7b43a44f
Etudiant <=>
2019-02-08 16:52:01 +0100

    todo :construction:

---
+++ models/Voiture.php
@@ -0,0 +1,15 @@
+<?php
+
+class Voiture extends Db{
+
+    protected $id;
+    protected $marque;
+    protected $modele;
+    protected $anneeMiseenLoc;
+    protected $plaqueImmat;
+    protected $couleur;
+
+
+
+
+}
