d0cf7c647c13be754e8eb3eaaca1c3043caa47cf
Caroline Chapeau <caroline2chapeau@gmail.com>
2019-03-11 11:17:15 +0100

    fixing navbar links

--- public/views/links.php
+++ public/views/links.php
@@ -1,14 +1,12 @@
 <ul class="nav navbar-nav">
     <li class="active"><a href="<?= url('/'); ?>">Accueil</a></li>
+    <?php  if (isset($_SESSION['admin'])) { ?>
     <li><a href="<?= url('voitures'); ?>">Nos voitures</a></li>
     <li><a href="<?= url('locations'); ?>">Locations enregistrées</a></li>
     <li><a href="<?= url('agence'); ?>">Agences</a></li>
     <li><a href="<?= url('conducteurs'); ?>">Conducteurs</a></li>
-    <li class="dropdown">
-        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Mon compte <b class="caret"></b></a>
-        <ul class="dropdown-menu">
-            <li><a href="<?= url('login'); ?>">Se connecter</a></li>
-            <li><a href="<?= url('admin'); ?>">Espace Admin</a></li>
-        </ul>
-    </li>
+        <li><a href="<?= url('admin'); ?>">Mon compte: <?= unserialize($_SESSION['admin'])->employee()->prenom() ?></a></li>
+        <li><a href="<?= url('logout'); ?>">Deconnexion</a></li>
+
+     <?php }?>
 </ul>
--- public/views/pages/404.php
+++ public/views/pages/404.php
@@ -1,6 +1,14 @@
 <?php ob_start(); ?>
 
-vous êtes perdus sur internet 
+
+
+
+
+
+<a class="btn btn-secondary my-5 btn-lg d-flex justify-content-center"  href="<?= url('signup') ?>">S'inscrire</a>
+    <a class="btn btn-secondary my-5 btn-lg d-flex justify-content-center"  href="<?= url('login') ?>">Se connecter</a>
+
+    
 <?php $content = ob_get_clean(); ?>
 
 <?php view('template', compact('content')); ?>
